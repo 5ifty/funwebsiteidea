457215f6468dfebf99a99615553ac08c262a44e5
Harry Howard <harry.howard@3gi.co.uk>
2023-10-20 08:23:36 +0100

    Clean up logic

--- index.py
+++ index.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template
 import sys
 
-
 sys.path.append('/')
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/', methods=['GET'])
-def index(self):
+def index():
     return render_template('HTML.html')
 
-Flask.register(app,route_base = '/')
-app.logger.removeHandler(default_handler)
-
-
 if __name__ == "__main__":
-  app.run(debug='True', host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
